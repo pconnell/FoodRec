0739a40be5737426ad3a4a6998c4e1fecb7159d6
Patrick Taitt <ptaitt@mail.umw.edu>
2010-11-01 19:57:45 -0400

    added a FoodTeam.php page that lists the members in the group

---
+++ FoodTeam.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>FoodRec</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+<div id="wrap">
+   	<div id="main">
+  
+	<?php
+	
+	echo "<p><span style=\"color:red\"><b>Food Team Members:</B></span><br/>";
+	include("header.php");
+
+	?>
+	<h4><a href=''>Patrick Connelly</a></h4>
+	<h4><a href=''>Thomas Cormier</a></h4>
+	<h4><a href=''>Zwe Maung</a></h4>
+	<h4><a href=''>Patrick Taitt</a></h4>
+	</div>	
+	
+    <?php include("projectSideBar.php"); ?>
+	<?php include("footer.html");?>
+</div>
+</div>
+</body>
+</html>
